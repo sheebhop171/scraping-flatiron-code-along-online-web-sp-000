bff2ccf0c135778d7e355c48652808790247f184
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-08 23:19:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,15 +18,7 @@ class Scraper
   end
 
   def get_page
-
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-    #doc.css(".post").each do |post|
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
-    #end
   end
 
   def get_courses
