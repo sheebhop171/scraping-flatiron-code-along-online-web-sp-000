e8307f560a054dec54e3c6bcedf3c8c5c441578e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-08 22:56:23 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,6 @@
 class Course
 
-  attr_accessor :title, :schedule, :description
+#  attr_accessor :title, :schedule, :description
 
-  def initialize(title, schedule, description)
+#  def initialize(title, schedule, description)
 end
