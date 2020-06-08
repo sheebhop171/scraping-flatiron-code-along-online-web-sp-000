df1c9a19d5d3adb2de96ac99ae387a8c8003be8a
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-08 23:03:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +15,9 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+
+  end
+
 end
-
-
-
